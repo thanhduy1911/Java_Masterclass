5edb8827782de05eb7282bd250c08c456dfa6e37
Thanh Duy <duythanh19111999@gmail.com>
2025-01-03 17:22:01 +0700

    Modified Homework: PositiveNegativeZero

--- src/section_5/PositiveNegativeZero.java
+++ src/section_5/PositiveNegativeZero.java
@@ -2,6 +2,11 @@ package section_5;
 
 public class PositiveNegativeZero {
     public static void main(String[] args) {
-
+        checkNumber(2);
+        checkNumber(-1);
+        checkNumber(0);
+    }
+    public static void checkNumber(int number) {
+        System.out.println((number > 0) ? "Positive" : (number < 0) ? "Negative" : "Zero");
     }
 }
