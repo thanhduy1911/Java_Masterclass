86d0733a5d2948b58c04e36c69efd445210d4634
Thanh Duy <duythanh19111999@gmail.com>
2025-02-05 14:31:19 +0700

    Inheritance Challenge

--- src/section_7/InheritanceChallenge/Main.java
+++ src/section_7/InheritanceChallenge/Main.java
@@ -1,4 +1,22 @@
 package section_7.InheritanceChallenge;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee duy = new Employee("Duy", "19/11/1999", "01/01/2020");
+        System.out.println(duy);
+        System.out.println("Age = " + duy.getAge());
+        System.out.println("Pay = " + duy.collectPay());
+
+        SalariedEmployee nhi = new SalariedEmployee("Nhi", "15/02/2001", "03/03/2020", 35000);
+        System.out.println(nhi);
+        System.out.println("Nhi's Paycheck = $" + nhi.collectPay());
+
+        nhi.retire();
+        System.out.println("Nhi's Pension check = $" + nhi.collectPay());
+
+        HourlyEmployee phu = new HourlyEmployee("Phu", "29/04/1997", "02/02/2021", 15);
+        System.out.println(phu);
+        System.out.println("Phu's Paycheck = $" + phu.collectPay());
+        System.out.println("Phu's Holiday Pay = $" + phu.getDoublePay());
+    }
 }
