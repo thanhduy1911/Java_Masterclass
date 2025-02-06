09ee43c81ba14191f7b336bd00c56784eff3f851
Thanh Duy <duythanh19111999@gmail.com>
2025-02-06 16:59:48 +0700

    Text Formatting Lesson

---
+++ src/section_7/StringFormatting/Main.java
@@ -0,0 +1,27 @@
+package section_7.StringFormatting;
+
+import java.time.Year;
+
+public class Main {
+    public static void main(String[] args) {
+        String bulletList = "Print a Bulleted List: \n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point";
+        System.out.println(bulletList);
+
+        String textBlock = """
+                Print a Bulleted List:
+                            \u2022 First Point
+                                \u2022 Sub Point""";
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = Year.now().getValue() - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n",(float) age);
+
+    }
+}
