76460685d93c366c583dbe470d30606cb83139a4
Thanh Duy <duythanh19111999@gmail.com>
2025-02-12 16:58:05 +0700

    Coding

---
+++ src/section_8/CompositionExercise/Wall.java
@@ -0,0 +1,13 @@
+package section_8.CompositionExercise;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
