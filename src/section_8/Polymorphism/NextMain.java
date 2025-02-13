173b67ff7831ab17bc92c621c5cbfddc7e0ab56b
Thanh Duy <duythanh19111999@gmail.com>
2025-02-13 16:59:40 +0700

    Learning

---
+++ src/section_8/Polymorphism/NextMain.java
@@ -0,0 +1,28 @@
+package section_8.Polymorphism;
+
+public class NextMain {
+    public static void main(String[] args) {
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Adventure.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+        airplane.watchMovie();
+
+        Object unknowObject = Movie.getMovie("C", "Airplane");
+        if (unknowObject.getClass().getSimpleName() == "Comedy") {
+            Comedy c = (Comedy) unknowObject;
+            c.watchComedy();
+        } else if (unknowObject instanceof Adventure) {
+            ((Adventure) unknowObject).watchAdventure();
+        } else if (unknowObject instanceof ScienceFiction syfy) {
+            syfy.watchScienceFiction();
+        }
+    }
+}
