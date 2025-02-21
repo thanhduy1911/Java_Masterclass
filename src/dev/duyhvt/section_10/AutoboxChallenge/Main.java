f12330d5a029ef140ea7b2bba4271f679df3f49a
Thanh Duy <duythanh19111999@gmail.com>
2025-02-21 15:32:22 +0700

    Hard work coding

---
+++ src/dev/duyhvt/section_10/AutoboxChallenge/Main.java
@@ -0,0 +1,89 @@
+package dev.duyhvt.section_10.AutoboxChallenge;
+
+import java.util.ArrayList;
+
+record Customer(String name, ArrayList<Double> transaction) {
+    public Customer(String name, double initialDeposit) {
+        this(name.toUpperCase(),
+                new ArrayList<>(500));
+        transaction.add(initialDeposit);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Customer bob = new Customer("Bob S", 1000.0);
+        System.out.println(bob);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("Bob S", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
+    }
+}
+
+class Bank {
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>(5000);
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
+    }
+
+    private Customer getCustomer(String customerName) {
+        for (var customer : customers) {
+            if (customer.name().equalsIgnoreCase(customerName)) {
+                return customer;
+            }
+        }
+
+        System.out.printf("Customer (%s) wasn't found.%n", customerName);
+        return null;
+    }
+
+    public void addNewCustomer(String customerName, double initialDeposit) {
+        if (getCustomer(customerName) == null) {
+            Customer customer = new Customer(customerName, initialDeposit);
+            customers.add(customer);
+            System.out.println("New customer added: " + customer);
+        }
+    }
+
+    public void addTransaction(String name, double transactionAmount) {
+        Customer customer = getCustomer(name);
+        if (customer != null) {
+            customer.transaction().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName) {
+        Customer customer = getCustomer(customerName);
+        if (customer == null) {
+            return;
+        }
+
+        System.out.println("-".repeat(30));
+        System.out.println("Customer name: " + customer.name());
+        System.out.println("Transaction: ");
+        System.out.println("-".repeat(30));
+
+        for (double d : customer.transaction()) { // unboxing
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+        }
+    }
+}
---
+++ src/dev/duyhvt/section_10/Bank/Bank.java
@@ -0,0 +1,67 @@
+package dev.duyhvt.section_10.Bank;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<>(500);
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialTransaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialTransaction);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customer, double transaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.addCustomerTransaction(customer, transaction);
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for (Branch branch : branches) {
+            if (branch.getName().equalsIgnoreCase(branchName)) {
+                return branch;
+            }
+        }
+        return null;
+    }
+
+    public boolean listCustomers(String branchName, boolean printTransactions) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customer details for branch " + branch.getName());
+            ArrayList<Customer> customers = branch.getCustomers();
+            for (int i = 0; i < customers.size(); i++) {
+                Customer customer = customers.get(i);
+                System.out.println("Customer: " + customer.getName() + "[" + (i + 1) + "]");
+                if (printTransactions) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = customer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
