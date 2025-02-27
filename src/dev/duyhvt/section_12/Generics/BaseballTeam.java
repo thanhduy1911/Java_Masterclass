ad3348b773e5cecd5b1adb6a3635addc7d968f52
Thanh Duy <duythanh19111999@gmail.com>
2025-02-27 17:01:08 +0700

    Section 12

---
+++ src/dev/duyhvt/section_12/Generics/BaseballTeam.java
@@ -0,0 +1,5 @@
+package dev.duyhvt.section_12.Generics;
+
+public class BaseballTeam {
+
+}
---
+++ src/dev/duyhvt/section_12/Generics/Main.java
@@ -0,0 +1,6 @@
+package dev.duyhvt.section_12.Generics;
+
+record BaseballPlayer(String name, String description) {}
+
+public class Main {
+}
