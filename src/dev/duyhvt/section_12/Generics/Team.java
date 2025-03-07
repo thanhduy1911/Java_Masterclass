287e9342fc43603e27fe7095e1ca7c8e4e29bec5
Thanh Duy <duythanh19111999@gmail.com>
2025-03-07 15:28:04 +0700

    Section 13

--- src/dev/duyhvt/section_12/Generics/Team.java
+++ src/dev/duyhvt/section_12/Generics/Team.java
@@ -3,15 +3,28 @@ package dev.duyhvt.section_12.Generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T> {
+record Affiliation(String name, String type, String countryCode) {
+    @Override
+    public String toString() {
+        return name + " (" + type + " in " + countryCode + ")";
+    }
+}
+
+public class Team<T extends Player, S> {
     private String teamName;
     private List<T> teamMembers = new ArrayList<>();
     private int totalWins = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
+    private S affiliation;
 
     public Team(String teamName) {
         this.teamName = teamName;
+    }
+
+    public Team(String teamName, S affiliation) {
+        this.teamName = teamName;
+        this.affiliation = affiliation;
     }
 
     public void addTeamMember(T t) {
@@ -21,8 +34,12 @@ public class Team<T> {
     }
 
     public void listTeamMembers() {
-        System.out.println(teamName + " Roster:");
-        System.out.println(teamMembers);
+        System.out.print(teamName + " Roster:");
+        System.out.println(affiliation == null ? "" : " Affiliation: ".toUpperCase() + affiliation);
+//        System.out.println(teamMembers);
+        for (T t : teamMembers) {
+            System.out.println(t.name());
+        }
     }
 
     public int ranking() {
