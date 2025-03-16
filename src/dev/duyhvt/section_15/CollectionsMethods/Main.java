d6d4a376314c75ab9b974f0f8db16553174b3930
Thanh Duy <duythanh19111999@gmail.com>
2025-03-16 23:21:18 +0700

    Section 15

---
+++ src/dev/duyhvt/section_15/CollectionsMethods/Main.java
@@ -0,0 +1,10 @@
+package dev.duyhvt.section_15.CollectionsMethods;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
