08976d458f1e99ac97ab75efcaa9d5699164eb26
Thanh Duy <duythanh19111999@gmail.com>
2025-03-27 16:53:52 +0700

    Set and Map

--- src/dev/duyhvt/section_15/SetsAndMaps/Main.java
+++ src/dev/duyhvt/section_15/SetsAndMaps/Main.java
@@ -1,7 +1,34 @@
 package dev.duyhvt.section_15.SetsAndMaps;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        List<Contact> emails = ContactData.getData("email");
+        List<Contact> phones = ContactData.getData("phone");
+        printData("Phone List", phones);
+        printData("Email List", emails);
 
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        printData("Phone List", phoneContacts);
+        printData("Email List", emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
+
+    }
+    public static void printData(String header, Collection<Contact> contacts) {
+        System.out.println("-".repeat(40));
+        System.out.println(header);
+        System.out.println("-".repeat(40));
+        contacts.forEach(System.out::println);
     }
 }
