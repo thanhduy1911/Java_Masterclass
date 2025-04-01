d7c056f73f17542cb19af45589d2d21a9e93c263
Thanh Duy <duythanh19111999@gmail.com>
2025-04-01 21:34:31 +0700

    HashSet, HashMap and TreeMap

---
+++ src/dev/duyhvt/section_15/FinalCollectionsChallenge/Category.java
@@ -0,0 +1,5 @@
+package dev.duyhvt.section_15.FinalCollectionsChallenge;
+
+public enum Category {
+    PRODUCE, DAIRY, CEREAL, MEAT, BEVERAGE
+}
---
+++ src/dev/duyhvt/section_15/FinalCollectionsChallenge/Product.java
@@ -0,0 +1,4 @@
+package dev.duyhvt.section_15.FinalCollectionsChallenge;
+
+public record Product(String sku, String name, String mfgr, Category category) {
+}
