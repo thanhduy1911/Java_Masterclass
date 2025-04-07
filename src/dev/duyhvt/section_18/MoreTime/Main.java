68e46547c5ef58a410c8e1064005b23185cb8ee5
Thanh Duy <duythanh19111999@gmail.com>
2025-04-07 12:30:07 +0700

    DateTime

---
+++ src/dev/duyhvt/section_18/MoreTime/Main.java
@@ -0,0 +1,62 @@
+package dev.duyhvt.section_18.MoreTime;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TimeZone;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("user.timezone", "America/Los_Angeles");
+        System.out.println(ZoneId.systemDefault());
+        System.out.println("Number of TZs = " + ZoneId.getAvailableZoneIds().size());
+        ZoneId.getAvailableZoneIds().stream()
+                .filter(s -> s.startsWith("Asia"))
+                .sorted()
+                .map(ZoneId::of)
+                .forEach(z -> System.out.println(z.getId() + ": " + z.getRules()));
+
+        Set<String> jdk8Zones = ZoneId.getAvailableZoneIds();
+        String[] alternate = TimeZone.getAvailableIDs();
+        Set<String> oldWay = new HashSet<>(Set.of(alternate));
+
+        oldWay.removeAll(jdk8Zones);
+        System.out.println(oldWay);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        Instant instantNow = Instant.now();
+        System.out.println(instantNow);
+
+        for (ZoneId z : List.of(
+                ZoneId.of("Australia/Sydney"),
+                ZoneId.of("Europe/Paris"),
+                ZoneId.of("America/New_York"))) {
+            DateTimeFormatter zoneFormat = DateTimeFormatter.ofPattern("z:zzzz");
+            System.out.println(z);
+            System.out.println("\t" + instantNow.atZone(z).format(zoneFormat));
+            System.out.println("\t" + z.getRules().getDaylightSavings(instantNow));
+            System.out.println("\t" + z.getRules().isDaylightSavings(instantNow));
+        }
+
+        Instant dobInstant = Instant.parse("2020-01-01T08:01:00Z");
+        LocalDateTime dob =
+                LocalDateTime.ofInstant(dobInstant, ZoneId.systemDefault());
+        System.out.println("Your kid's birthdate, LA Time = " + dob.format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        ));
+
+        ZonedDateTime dobSydney = ZonedDateTime.ofInstant(dobInstant,
+                ZoneId.of("Australia/Sydney"));
+        System.out.println("Your kid's birthdate, Sydney Time = " + dobSydney.format(
+                DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+        ));
+    }
+}
