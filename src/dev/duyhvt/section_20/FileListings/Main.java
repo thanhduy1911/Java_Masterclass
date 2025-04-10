57ed3e564dd3a0ae94402148577063d16e9172fd
Thanh Duy <duythanh19111999@gmail.com>
2025-04-10 16:59:22 +0700

    IO Handling

--- src/dev/duyhvt/section_20/FileListings/Main.java
+++ src/dev/duyhvt/section_20/FileListings/Main.java
@@ -20,6 +20,45 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        System.out.println("-".repeat(60));
+        try (Stream<Path> paths = Files.walk(path, 2)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("-".repeat(60));
+        try (Stream<Path> paths = Files.find(path, Integer.MAX_VALUE,
+                (p, attr) -> attr.isRegularFile() && attr.size() > 300
+        )) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        path = path.resolve(".idea");
+
+        System.out.println("=".repeat(10) + "Directory Stream" + "=".repeat(10));
+        try (var dirs = Files.newDirectoryStream(path,
+                p -> p.getFileName().toString().endsWith(".xml")
+                && Files.isRegularFile(p) && Files.size(p) > 1000)) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("=".repeat(10) + "Directory Stream" + "=".repeat(10));
+        try (var dirs = Files.newDirectoryStream(path, "*.xml")) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static String listDir(Path path) {
