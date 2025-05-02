b64ed2de009e356e96260ef1ccdf26952ff606c9
Thanh Duy <duythanh19111999@gmail.com>
2025-05-02 16:56:39 +0700

    leetcode 75

---
+++ src/dev/exercises/leetcode/leetcode75/array_string/quiz1/Main.java
@@ -0,0 +1,15 @@
+package dev.exercises.leetcode.leetcode75.array_string.quiz1;
+
+public class Main {
+    public static void main(String[] args) {
+        // two pointer approach
+
+
+    }
+
+    private String mergeAlternately(String word1, String word2) {
+
+
+        return "";
+    }
+}
