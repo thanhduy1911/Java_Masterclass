81d2a4c904bbfef0d60b7c354d08a73a086759f1
Thanh Duy <duythanh19111999@gmail.com>
2025-10-24 23:29:45 +0700

    Doing some POTD Leetcode

---
+++ src/dev/duyhvt/coding/checkIfDigitsAreEqualInStringAfterOperationsI.java
@@ -0,0 +1,20 @@
+package dev.duyhvt.coding;
+
+public class checkIfDigitsAreEqualInStringAfterOperationsI {
+    private static boolean hasSameDigits(String s) {
+        while (s.length() > 2) {
+            StringBuilder next = new StringBuilder();
+            for (int i = 0; i < s.length() - 1; i++) {
+                int sum = (s.charAt(i) + s.charAt(i + 1)) % 10;
+                next.append(sum);
+            }
+            s = next.toString();
+        }
+
+        return s.charAt(0) == s.charAt(1);
+    }
+    public static void main(String[] args) {
+        System.out.println(hasSameDigits("3902"));
+        System.out.println(hasSameDigits("34789"));
+    }
+}
