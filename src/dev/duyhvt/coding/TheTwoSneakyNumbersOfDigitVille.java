00a4cf4b73a35ffff900e8adba00c0ef559b85e3
Thanh Duy <duythanh19111999@gmail.com>
2025-10-31 17:43:15 +0700

    Doing POTD Leetcode

---
+++ src/dev/duyhvt/coding/TheTwoSneakyNumbersOfDigitVille.java
@@ -0,0 +1,24 @@
+package dev.duyhvt.coding;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TheTwoSneakyNumbersOfDigitVille {
+    private static int[] solution(int[] nums) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int num : nums) {
+            seen.put(num, seen.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> result = seen.entrySet().stream()
+                .filter(e -> e.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .toList();
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+    public static void main(String[] args) {
+
+    }
+}
