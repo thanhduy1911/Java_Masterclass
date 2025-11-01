b8cafade65d0e84bb5b4c5de4de2c400620d6606
Thanh Duy <duythanh19111999@gmail.com>
2025-11-01 16:27:38 +0700

    Doing POTD Leetcode

---
+++ src/dev/duyhvt/coding/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,25 @@
+package dev.duyhvt.coding;
+
+public class DeleteNodesFromLinkedListPresentInArray {
+//    private static ListNode modifiedList(int[] nums, ListNode head) {
+//        Set<Integer> valuesToRemove = new HashSet<>();
+//        for (int num : nums) {
+//            valuesToRemove.add(num);
+//        }
+//
+//        while (head != null && valuesToRemove.contains(head.val)) {
+//            head = head.next;
+//        }
+//
+//        ListNode curr = head;
+//        while (curr.next != null) {
+//            if (valuesToRemove.contains(curr.bext.val)) {
+//                curr.next = curr.next.next;
+//            } else {
+//                curr = curr.next;
+//            }
+//        }
+//
+//        return head == null ? null : head;
+//    }
+}
