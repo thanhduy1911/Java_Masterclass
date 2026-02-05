58dd79cf6f91f05dbdc10be3b775271183e71f16
Thanh Duy <drthanhduy@gmail.com>
2026-02-05 17:18:32 +0700

    feat(DUY-0502): 367. Hands-On JDBC: Building a Database Application with Transaction & Data Operation

--- src/dev/duyhvt/section_22/JDBCChallenge/Main.java
+++ src/dev/duyhvt/section_22/JDBCChallenge/Main.java
@@ -1,11 +1,10 @@
 package dev.duyhvt.section_22.JDBCChallenge;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import dev.duyhvt.utilities.exceptions.MySQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
   private static final String USE_SCHEMA;
@@ -29,6 +28,10 @@ public class Main {
         System.out.println("storefront schema does not exist");
         setUpSchema(connection);
       }
+
+      //      int newOrder = addOrder(connection, new String[] {"shoes", "shirt", "socks"});
+      //      System.out.println("New Order = " + newOrder);
+      deleteOrder(connection, 1);
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -85,6 +88,62 @@ REFERENCES storefront.orders (order_id) ON DELETE CASCADE)
 
     } catch (SQLException e) {
       System.err.println("Message: " + e.getMessage());
+    }
+  }
+
+  private static int addOrder(Connection connection, String[] items) throws SQLException {
+    int orderId = -1;
+    String insertOrder = "INSERT INTO storefront.orders (order_date) VALUES ('%s')";
+    String insertDetails =
+        "INSERT INTO storefront.order_details (order_id, item_description) VALUES (%d, %s)";
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    String orderDateTime = LocalDateTime.now().format(dateTimeFormatter);
+    System.out.println(orderDateTime);
+    String formattedString = insertOrder.formatted(orderDateTime);
+    System.out.println(formattedString);
+
+    try (Statement statement = connection.createStatement()) {
+      connection.setAutoCommit(false);
+      int inserts = statement.executeUpdate(formattedString, Statement.RETURN_GENERATED_KEYS);
+      if (inserts == 1) {
+        var generatedKeys = statement.getGeneratedKeys();
+        if (generatedKeys.next()) {
+          orderId = generatedKeys.getInt(1);
+        }
+      }
+      int count = 0;
+      for (var item : items) {
+        formattedString = insertDetails.formatted(orderId, statement.enquoteLiteral(item));
+        inserts = statement.executeUpdate(formattedString);
+        count += inserts;
+      }
+      if (count != items.length) {
+        orderId = -1;
+        System.out.println("Number of records inserted doesn't equal to items received");
+        connection.rollback();
+      } else {
+        connection.commit();
+      }
+      connection.setAutoCommit(true);
+    } catch (SQLException e) {
+      connection.rollback();
+
+      throw new MySQLException(e.getMessage());
+    }
+    return orderId;
+  }
+
+  private static void deleteOrder(Connection connection, int orderId) throws SQLException {
+    String deleteOrder = "DELETE FROM storefront.orders WHERE order_id = %d";
+    String deleteQuery = deleteOrder.formatted(orderId);
+
+    try (Statement statement = connection.createStatement()) {
+      int deletedRecords = statement.executeUpdate(deleteQuery);
+      System.out.printf("%d records deleted\n", deletedRecords);
+    } catch (SQLException e) {
+      connection.rollback();
+      throw new MySQLException(e.getMessage());
     }
   }
 }
